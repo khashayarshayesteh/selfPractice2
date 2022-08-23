3d3164ef221ddbf6e604ce32423d19b3b4586447
khashayar shayesteh <shayesteh.office@gmail.com>
2022-08-23 07:18:18 -0700

    first commit

--- src/main/java/day90_lambda/InsuranceTest.java
+++ src/main/java/day90_lambda/InsuranceTest.java
@@ -1,4 +1,21 @@
 package day90_lambda;
 
-public class InsuranceTest {
+public class InsuranceTest  {
+    public static void main(String[] args) {
+
+        Person karim = new Person();
+        Car honda = new Car();
+        Insurance Gaico = new Insurance();
+
+        getCarInsuranceName(karim);
+
+
+
+
+    }
+
+        public static String getCarInsuranceName(Person person){
+            return person.getCar().getInsurance().getName();
+
+    }
 }
