7d09b3b63df1315e237f9a27640bdb7930d5695e
khashayar shayesteh <shayesteh.office@gmail.com>
2022-11-18 19:49:46 -0800

    GreatLearning

--- src/main/java/greatLearning/StackSolution.java
+++ src/main/java/greatLearning/StackSolution.java
@@ -1,5 +1,6 @@
 package greatLearning;
 
+import java.util.Iterator;
 import java.util.Stack;
 
 public class StackSolution {
@@ -9,10 +10,27 @@ public class StackSolution {
         s1.push(2);
         s1.push(4);
         s1.push(9);
+        int indexFound = s1.search(9);
+        System.out.println("indexFound is: " + indexFound);
         System.out.println(s1);
+        Iterator it = s1.iterator();
+        while(it.hasNext()){
+            Object s2 = it.next();
+            System.out.print(s2);
+        }
+
+        System.out.println();
 
         s1.pop();
         System.out.println(s1);
+        System.out.println(s1.peek());
+        System.out.println(s1);
+        boolean status = s1.empty();
+        System.out.println(status);
+        s1.pop();
+        s1.pop();
+        boolean status1 = s1.empty();
+        System.out.println(status1);
 
 
 
