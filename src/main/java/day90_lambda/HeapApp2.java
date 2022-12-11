dce7d1303194f2e91c2361ca43d1e94c5a48296d
khashayar shayesteh <shayesteh.office@gmail.com>
2022-12-10 21:20:14 -0800

    Heap

--- src/main/java/day90_lambda/HeapApp2.java
+++ src/main/java/day90_lambda/HeapApp2.java
@@ -3,6 +3,13 @@ package day90_lambda;
 public class HeapApp2 {
     public static void main(String[] args){
         MyHeap heap = new MyHeap(10);
+        heap.insert(61);
+        heap.insert(120);
+        heap.insert(52);
+        heap.insert(45);
+        heap.insert(81  );
+
+        heap.printHead();
 
 
 
--- src/main/java/day90_lambda/MyHeap.java
+++ src/main/java/day90_lambda/MyHeap.java
@@ -1,7 +1,59 @@
 package day90_lambda;
 
-public class MyHeap {
-    int
+import java.util.NoSuchElementException;
 
+public class MyHeap {
+    int[] items;
+    int size;
+
+    public MyHeap(int capacity) {
+        this.items = new int[capacity];
+        this.size = 0;
+    }
+
+    public int peak() {
+        if (size == 0) throw new NoSuchElementException();
+        return items[0];
+    }
+
+    public void bubbleUp() {
+        int index = size - 1;
+
+
+        while (index > 0 && items[index] > items[
+
+                parentIndex(index)]) {
+            swap(index, parentIndex(index));
+            index = parentIndex(index);
+        }
+    }
+
+
+    public void insert(int value) {
+        if (size == items.length) throw new IllegalStateException();
+        else {
+            items[size++] = value;
+            bubbleUp();
+        }
+    }
+
+    public void swap(int first, int second) {
+        int temp = items[first];
+        items[first] = items[second];
+        items[second] = temp;
+
+    }
+
+    public int parentIndex(int index) {
+        return (index - 1) / 2;
+    }
+
+    public void printHead() {
+        for (int i = 0; i < size; i++) {
+            System.out.print(items[i] + ", ");
+
+        }
+    }
 
 }
+
